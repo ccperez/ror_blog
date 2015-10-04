bb55116d93ab7642bcefe7c2478810bbd7efd7aa
Conrad Perez <conrad.perez.dev@gmail.com>
2015-10-04 20:19:11 +0800

    setup gmail production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'https://ror-aps-blg.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV["GMAIL_USERNAME"],
+    :password       => ENV["GMAIL_PASSWORD"],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
 end
